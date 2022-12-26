eda9a42e88485761a74a1791eedb29acf5f91933
Rishi Prakash Soni <avviare.rishi@gmail.com>
2022-12-26 22:16:22 +0530

    skill Bar Fixed

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let number = document.getElementById('number_per')
 let number1 = document.getElementById('number_per1')
 let number2 = document.getElementById('number_per2')
+let number3 = document.getElementById('number_per3')
+let number4 = document.getElementById('number_per4')
 let counter = 0;
 let counter1 = 0;
 let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
 setInterval(() => {
-    if (counter == 65) {
+    if (counter == 35) {
         clearInterval();
     } else {
         counter += 1;
@@ -14,7 +18,7 @@ setInterval(() => {
 
 }, 15)
 setInterval(() => {
-    if (counter1 == 75) {
+    if (counter1 == 45) {
         clearInterval();
     } else {
         counter1 += 1;
@@ -23,11 +27,29 @@ setInterval(() => {
 
 }, 15)
 setInterval(() => {
-    if (counter2 == 85) {
+    if (counter2 == 65) {
         clearInterval();
     } else {
         counter2 += 1;
         number2.innerHTML = counter2 + '%';
+    }
+
+}, 15)
+setInterval(() => {
+    if (counter3 == 20) {
+        clearInterval();
+    } else {
+        counter3 += 1;
+        number3.innerHTML = counter3 + '%';
+    }
+
+}, 15)
+setInterval(() => {
+    if (counter4 == 60) {
+        clearInterval();
+    } else {
+        counter4 += 1;
+        number4.innerHTML = counter4 + '%';
     }
 
 }, 15)
