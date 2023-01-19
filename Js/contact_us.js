19bd2fd28b172092820bb62c05930a9867b8ecc7
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-01-19 03:53:46 +0000

    Slider bar added

--- Js/contact_us.js
+++ Js/contact_us.js
@@ -1,45 +1,19 @@
-// Get references to the form elements
-var form = document.getElementById("contact-form");
-var nameInput = document.getElementById("fullName");
-var emailInput = document.getElementById("emailId");
-var messageInput = document.getElementById("userText");
+function sendEmail() {
+    var name = $('#fullName').val();
+    var email = $('#emailId').val();
+    var message = $('#userText').val();
 
-// Listen for form submission
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    // Get the name, email, and message values
-    var name = nameInput.value;
-    var email = emailInput.value;
-    var message = messageInput.value;
-
-    // Perform validation
-    if (!name) {
-        alert("Please enter your name.");
-        return;
-    }
-
-    if (!email) {
-        alert("Please enter your email address.");
-        return;
-    }
-
-    if (!message) {
-        alert("Please enter a message.");
-        return;
-    }
-
-    // If validation passes, send the data as an email
-    email.send({
-        Host: "smtp.gmail.com",
-        Username: "rishi1.edunext@gmail.com",
-        Password: "E26EAAE9C3BFCF727285484DE0FC2E2F0125",
-        // SecureToken: "d6d2076a-3f2f-45e2-9045-a8b0032fe012",
-        To: 'rishi.edunext@gmail.com',
-        From: email,
-        Subject: "New Contact Form Submission",
-        Body: message
-    }).then(
-        message => alert("Your message has been sent!")
-    );
-});
+    var body = 'Name: ' + name + '<br>Email: ' + email + '<br>Message: ' + message;
+}
+Email.send({
+    // SecureToken: "6525fcc5-7a42-4f05-ae09-70bcaa9a3946",
+    Host: "smtp.elasticemail.com",
+    Username: "avviare.rishi@gmail.com",
+    Password: "C23F008103F730AF9D554BFDD4D1A365EED6",
+    To: 'rishi.edunext@gmail.com',
+    From: "avviare.rishi@gmail.com",
+    Subject: "This is the subject",
+    Body: body
+}).then(
+    message => alert(message)
+);
