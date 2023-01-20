8f70bb74230b753430e8ceb8c0b4c26d553edcd7
Rishi Prakash Soni <avviare.rishi@gmail.com>
2023-01-20 09:46:00 +0530

    F

--- Js/JS Card/script.js
+++ Js/JS Card/script.js
@@ -1,11 +1,29 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-  items: 4,
-  // items change number for slider display on desktop
-
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
   loop: true,
-  margin: 10,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  autoplayHoverPause: true
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
 });
