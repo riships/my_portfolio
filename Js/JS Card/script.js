c286169ab03581b339aa208b21fadf37acf656e2
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-01-20 04:10:19 +0000

    Ohk

--- Js/JS Card/script.js
+++ Js/JS Card/script.js
@@ -1,29 +1,11 @@
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  items: 4,
+  // items change number for slider display on desktop
 
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true
 });
