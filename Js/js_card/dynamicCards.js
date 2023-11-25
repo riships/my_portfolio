c0b4561f42ddeaba0247707561812f8d4f274796
riships <avviare.rishi@gmail.com>
2023-11-25 12:57:20 +0530

    improvement

--- Js/js_card/dynamicCards.js
+++ Js/js_card/dynamicCards.js
@@ -10,25 +10,27 @@ fetch('Js/js_card/cardsData.json') // Replace 'data.json' with the path to your 
             const firstDiv = document.createElement('div');
             const secondDiv_1 = document.createElement('div');
             const secondDiv_2 = document.createElement('div');
-            const imgSlid = document.createElement('img');
             const headIngThree = document.createElement('h3')
             const descPara = document.createElement('p');
             const linksDiv = document.createElement('div');
 
-
             // Set some attributes or properties for the new div
-            // newDiv.id = 'myNewDiv';
             firstDiv.className = 'card swiper-slide';
-            // firstDiv.textContent = 'Hello this is demo'
             secondDiv_1.className = 'card-header';
             secondDiv_2.className = 'card-body';
-            // heading part content and styling
             headIngThree.className = 'tag tag-teal';
             headIngThree.textContent = slide.title;
             descPara.textContent = slide.desc;
-            // imgSlid.setAttribute = ('width', '100%');
-            imgSlid.src = slide.imgUrl; // Use direct property assignment
-            imgSlid.alt = slide.title;
+
+
+
+
+            const vdoTag = `
+            <video id="myVideo" width="100%" autoplay muted>
+                <source src="${slide.imgUrl}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+            `
 
             // linksAdd.href = slide.repolink[1];
             const linksAdd = document.createElement('a');
@@ -40,7 +42,7 @@ fetch('Js/js_card/cardsData.json') // Replace 'data.json' with the path to your 
             linksDiv.appendChild(linksAdd2);
 
             linksAdd.innerText = 'Repo';
-            linksAdd2.innerText = 'Preview';
+            linksAdd2.innerText = 'Live';
 
 
 
@@ -56,7 +58,7 @@ fetch('Js/js_card/cardsData.json') // Replace 'data.json' with the path to your 
             secondDiv_2.appendChild(headIngThree);
             secondDiv_2.appendChild(descPara);
             secondDiv_2.appendChild(linksDiv);
-            secondDiv_1.appendChild(imgSlid);
+            secondDiv_1.innerHTML = vdoTag;
         })
     }).catch(error => console.error('Error fetching data:', error));
 
